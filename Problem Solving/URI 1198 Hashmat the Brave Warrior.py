891df39e7fe27d084859fb1578f79f336e79e253
shidhu <meatcse572@gmail.com>
2017-06-30 04:30:45 +0600

    Create URI 1198 Hashmat the Brave Warrior.py

---
+++ Problem Solving/URI 1198 Hashmat the Brave Warrior.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 29 22:17:28 2017
+
+@author: Pranta Sarker
+"""
+
+
+while True:
+    try:
+        a , b = raw_input().split(' ');
+        x = int(a);
+        y = int(b);
+        if x > y:
+            ans = x - y;
+        else:
+            ans = y - x;
+        print ans;
+    except EOFError:
+        break;
